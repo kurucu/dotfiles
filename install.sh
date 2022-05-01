77f1a4c1f950ad785353e362d48f1b05121bd172
Elliot Ali <git@elliotali.com>
2022-05-01 15:11:26 +1000

    Cleaning up

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 echo "Setting up your Mac..."
 
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
-
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -19,11 +14,8 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
-# Install PHP extensions with PECL
-pecl install imagick memcached redis swoole
+# Install PHP extensions with Pickle
+pickle install imagick memcached redis swoole
 
 # Install global Composer packages
 /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose
@@ -43,13 +35,6 @@ mkdir $HOME/Developer/Nova
 $HOME/.composer/vendor/bin/valet install
 $HOME/.composer/vendor/bin/valet park $HOME/Sites
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source .macos
+# source .macos
